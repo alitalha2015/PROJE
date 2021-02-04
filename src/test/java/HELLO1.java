f956e8af86bd2092378620208c2723609db54bb9
alitalha2015 <meltemozdincer@gmail.com>
2021-02-04 20:55:42 +0000

    INITial commit from intelliJ

--- src/test/java/HELLO1.java
+++ src/test/java/HELLO1.java
@@ -3,6 +3,6 @@ public class HELLO1 {
     public static void main(String[] args) {
         System.out.println("meltem");
         System.out.println("OZD");
-
+        System.out.println("ALI");
     }
 }
